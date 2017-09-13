20420ce9430645c4ad6442bb44633c69ba3a473d
igbt6 <lukasz.uszko@gmail.com>
2017-09-14 00:43:25 +0200

    Working fully system with time updated

--- RxModuleTI/ui/ui_screenMain.c
+++ RxModuleTI/ui/ui_screenMain.c
@@ -190,8 +190,8 @@ static void uiUpdateClock()
 		{
 			if (timeNow() != 0)
 			{
-				//int year = yearNow(); //update cache
-				//sprintf(ui_timeBuf, "%d-%02d-%02d  %02d:%02d", year, timeCurrentData().Month, timeCurrentData().Day, timeCurrentData().Hour, timeCurrentData().Minute);
+				int year = yearNow(); //update cache
+				sprintf(ui_timeBuf, "%d-%02d-%02d  %02d:%02d", year, timeCurrentData().Month, timeCurrentData().Day, timeCurrentData().Hour, timeCurrentData().Minute);
 			}
 		}
 		WidgetPaint((tWidget*)&ui_timeCanvas);
